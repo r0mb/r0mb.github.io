9f15940b2fd2f3026da0763199ae9c0318274f05
r0mb <33809599+r0mb@users.noreply.github.com>
2019-12-22 16:00:39 +0300

    Add files via upload

---
+++ literature/script.js
@@ -0,0 +1,10 @@
+function openn(obj) {
+document.getElementById("main2").src=document.getElementById(obj.id).src
+}
+function openn1(obj) {
+document.getElementById('main1').src=document.getElementById(obj.id).src
+}
+
+function openn2(obj) {
+document.getElementById('main2').src=document.getElementById(obj.id).src
+}
